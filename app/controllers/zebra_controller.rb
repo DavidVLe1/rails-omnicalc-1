95158dfed56f26a535b1fa32dccf585b0592acf7
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-07 08:25:47 +0000

    adjusted html and added functionality for square root.

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -12,4 +12,13 @@ class ZebraController < ApplicationController
     @the_result = @the_num.to_f ** 2
     render({ :template => "calculator_templates/square_result" })
   end
+
+  def square_root
+    render({ :template => "calculator_templates/square_root" })
+  end
+  def square_root_result
+    @the_num=params.fetch("number")
+    @the_result= @the_num.to_f ** 0.5
+    render({ :template => "calculator_templates/square_root_result" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/",{:controller => "zebra", :action => "home"})
   get("/square/new",{:controller => "zebra", :action => "square"})
   get("/square/results",{:controller => "zebra", :action => "square_result"})
+  get("/square_root/new",{:controller => "zebra", :action => "square_root"})
+  get("/square_root/results",{:controller => "zebra", :action => "square_root_result"})
 end
