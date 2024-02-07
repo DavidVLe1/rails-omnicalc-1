35dfa00beb9d5cc8d4bf2b720564fd2cf04b526e
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-07 06:42:11 +0000

    changed name of sqrt file to square, added route, controller and erb for square results.

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,8 +1,15 @@
 class ZebraController < ApplicationController
   def home
-   render({ :template => "calculator_templates/sqrt" })
+    render({ :template => "calculator_templates/square" })
   end
+
   def square
-    render({ :template => "calculator_templates/sqrt" })
+    render({ :template => "calculator_templates/square" })
+  end
+
+  def square_result
+    @the_num = params.fetch("number")
+    @the_result = @the_num.to_f ** 2
+    render({ :template => "calculator_templates/square_result" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get("/",{:controller => "zebra", :action => "home"})
-  get("/square/new",{:controller => "zebra", :action => "home"})
+  get("/square/new",{:controller => "zebra", :action => "square"})
+  get("/square/results",{:controller => "zebra", :action => "square_result"})
 end
