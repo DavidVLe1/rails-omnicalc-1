dc2d52c38dec1d354f6a1763092cc499204f09f9
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-07 09:14:09 +0000

    created functionality for payment

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -21,4 +21,14 @@ class ZebraController < ApplicationController
     @the_result= @the_num.to_f ** 0.5
     render({ :template => "calculator_templates/square_root_result" })
   end
+  def payment
+    render({ :template => "calculator_templates/payment" })
+  end
+  def payment_result
+    @apr=params.fetch("user_apr").to_f
+    @num_of_years=params.fetch("user_years").to_i
+    @principal=params.fetch("user_pv").to_f
+    @payment=(@apr/100 * @principal)/(1-(1+@apr/100)**(-12*@num_of_years))
+    render({ :template => "calculator_templates/payment_result" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/square/results",{:controller => "zebra", :action => "square_result"})
   get("/square_root/new",{:controller => "zebra", :action => "square_root"})
   get("/square_root/results",{:controller => "zebra", :action => "square_root_result"})
+  get("/payment/new",{:controller => "zebra", :action => "payment"})
+  get("/payment/results",{:controller => "zebra", :action => "payment_result"})
 end
