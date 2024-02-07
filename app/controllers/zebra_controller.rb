9d3530532eff5331ee24ba7309f367bc24b90624
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-07 09:22:03 +0000

    added random functionality.

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -16,19 +16,33 @@ class ZebraController < ApplicationController
   def square_root
     render({ :template => "calculator_templates/square_root" })
   end
+
   def square_root_result
-    @the_num=params.fetch("number")
-    @the_result= @the_num.to_f ** 0.5
+    @the_num = params.fetch("number")
+    @the_result = @the_num.to_f ** 0.5
     render({ :template => "calculator_templates/square_root_result" })
   end
+
   def payment
     render({ :template => "calculator_templates/payment" })
   end
+
   def payment_result
-    @apr=params.fetch("user_apr").to_f
-    @num_of_years=params.fetch("user_years").to_i
-    @principal=params.fetch("user_pv").to_f
-    @payment=(@apr/100 * @principal)/(1-(1+@apr/100)**(-12*@num_of_years))
+    @apr = params.fetch("user_apr").to_f
+    @num_of_years = params.fetch("user_years").to_i
+    @principal = params.fetch("user_pv").to_f
+    @payment = (@apr / 100 * @principal) / (1 - (1 + @apr / 100) ** (-12 * @num_of_years))
     render({ :template => "calculator_templates/payment_result" })
+  end
+
+  def random
+    render({ :template => "calculator_templates/random" })
+  end
+
+  def random_result
+    @minimum = params.fetch("Minimum").to_f
+    @maximum = params.fetch("Maximum").to_f
+    @chosen_number = rand(@minimum..@maximum)
+    render({ :template => "calculator_templates/random_result" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get("/square_root/results",{:controller => "zebra", :action => "square_root_result"})
   get("/payment/new",{:controller => "zebra", :action => "payment"})
   get("/payment/results",{:controller => "zebra", :action => "payment_result"})
+
+  get("/random/new",{:controller => "zebra", :action => "random"})
+  get("/random/results",{:controller => "zebra", :action => "random_result"})
 end
